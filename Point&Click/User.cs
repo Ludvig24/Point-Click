2b6ebe970eca4f2579151a81027797aefabdae19
Laura Marie Balle Jensen <laje11@student.eadania.dk>
2025-11-05 12:08:03 +0100

    Kommentar på User

--- Point&Click/User.cs
+++ Point&Click/User.cs
@@ -9,51 +9,57 @@ namespace Point_Click
 {
     internal class User 
     {
+        //Her er vores fields for vores klasse User
+        //Fields name og isDead benyttes ikke i spiller, men vi har valgt at lade dem blive, da vi oprindeligt vil benytte dem
+        //Eks, vil vi have brugt isDead, til at have fjener og at man kunne dø.
         private string name;
         private bool isDead;
+
+        //Denne field bruges i spillet.
         private Inventar inventar = new Inventar();
-    
-        public void Move(int roomID, User user, Room room1, Room room2, Room room3, List<Item> AllItems) //vi tager roomID som parameter til at bruge i switch - vi tager vores instans af MainWindow som vi sender med så vi kan bruge de objekter der er initialiseret i MainWindowet i vores andre vinduer
+
+        //Metode Move, går så brugeren kan bevæges sig mellem de forskellige rum.
+        //Vi tager roomID, user, rooms og liste som parameter.
+        public void Move(int roomID, User user, Room room1, Room room2, Room room3, List<Item> AllItems) 
         {
+            //Formålet med switchen er at finde frem til det rum vi gerne vil flytte til via roomID.
             switch (roomID)
             {
                 case 1:
-                    //MainWindow mainW = (MainWindow)window; //vi konverterer her window objektet til et MainWindow objekt vha cast (MainWindow) vi konverterer eksplicit window objekt til MainWindow
+                    //Vi laver et objekt af vores room1Window har understående parameter.
                     Room1Window room1W = new Room1Window(user, room1, room2, room3, AllItems);
+                    //Her vises rummet
                     room1W.Show();
                     
-                    
                     break;
+
                 case 2:
-                    //Room1Window room1W = (Room1Window)window;
                     Room2Window room2W = new Room2Window(user, room1, room2, room3, AllItems);
                     room2W.Show();
                     
                     break;
+
                 case 3:
                     Endscreen Endscreen = new Endscreen();
                     Endscreen.Show();
                     
                     break;
+
                 case 4:
                     MainWindow mainWindow = new MainWindow();
                     mainWindow.Show();
 
                     break;
             }
-            //formålet er at returnere et roomID som vi kan bruge til at navigere mellem de rum vi har oprettet
+            
         }
 
-        public Window Menu()
-        {
-            return null; //skal returnerer et Menu Window
-        }
-
+        //Her laver vi Get og Set, så vi kan anvende inventar i de andre klasser.
         public Inventar GetInventar()
         {
             return inventar;
         }
-
+  
         public void SetInventar(Inventar inventar)
         {
             this.inventar = inventar;
